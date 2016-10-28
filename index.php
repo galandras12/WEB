3c8f2130bda0a6e7933fed620d7177758edd498b
galandras12 <galandras12@gmail.com>
2016-10-28 08:00:46 +0200

    WEB upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>AngularJS and FabricJS</title>
+<script src='js/jquery.min.js'></script>
+<script src='js/fabric.min.js'></script>
+<script src='js/angular.min.js'></script>
+<script src='js/bootstrap.min.js'></script>
+<script src='js/plus.js'></script>
+<link rel='stylesheet prefetch' href='css/bootstrap.min.css'>
+<link rel='stylesheet prefetch' href='css/font-awesome.min.css'>
+<link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+
+<canvas id="c"></canvas>
+<img src="img/gif.PNG" id="my-image">
+
+  </body>
+</html>
---
+++ source/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/michaeljcalkins/pen/Imupw.
+
+ An example of binding AngularJS to a FabricJS canvas by using a service as a way of mimicking $scope for FabricJS.
